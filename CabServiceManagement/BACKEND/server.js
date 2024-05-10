a54c8efcf5b88159d2086b1fc9052dca11afd0d8
Bhawani Adithya <165871969+Bhawi5260@users.noreply.github.com>
2024-05-11 00:56:22 +0530

    bhawi2

---
+++ CabServiceManagement/BACKEND/server.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/v1/user", userRoutes);
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+
+    //useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    //useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("Mongodb Connection Success!");
+});
+const tripRouter = require("./routes/trips.js");
+app.use("/trip",tripRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number : ${PORT}`)
+});
---
+++ CabServiceManagement/frontend/src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'; 
+import AddTrip from './components/AddTrip';
+import AllTrips from './components/AllTrips';
+import Payment from './components/Payment';
+import Report from './components/Report';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Update from './components/Update';
+
+function App() {
+  const {loading}=useSelector((state)=>state.alerts);
+
+  useEffect(() => {
+    fetch('http://localhost:8070/trip/')
+      .then((response) => response.json())
+      .catch((error) => console.error('Error fetching trips:', error));
+  }, []);
+
+  return (
+    <Router>
+      {loading ? (
+        null
+      ) : (
+        <div>
+          <Routes>
+            <Route path="/" element={<AllTrips />} />
+            <Route path="/addTrip" element={<AddTrip />} />
+            <Route path="/payment/:id" element={<Payment/>} />
+            <Route path="/report" element={<Report/>} />
+            <Route path="/update/:id" element={<Update />} />
+          </Routes>
+        </div>
+      )}
+    </Router>
+  );
+}
+
+export default App;
