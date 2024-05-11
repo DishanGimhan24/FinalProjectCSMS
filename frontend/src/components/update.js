6e1d01fb006acf4c444c21a16119773b447b12c0
DishanGimhan24 <kdgimhan@gmail.com>
2024-05-11 16:11:59 +0530

    changes

--- frontend/src/components/update.js
+++ frontend/src/components/update.js
@@ -1,143 +1,241 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import "./update.css"; // Import the CSS file
-import logo from "../images/logo.jpg"; 
-import Navbar from "./Navbar";
+import React, { useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom';
+import "../styles/UpdateStyle.css";
+import Axios from 'axios';
 
-export default function UpdateProfile() {
-  const [userDetails, setUserDetails] = useState({
-    name: "",
-    age: "",
-    type: "",
-    address: "",
-    drivingExperience: "", 
-    licenseYear: "", 
+function Update() {
+  const{id}=useParams();
+  const location = useLocation();
+  const [values,setValues]=useState({
+    Firstname:'',
+    Lastname:'',
+    Address:'',
+    ContactNo:'',
+    Destination:'',
+    VisitingPlaces:'',
+    NumberOfPassengers:'1',
+    VehicleType:'',
+    date:'',
+    time:'',
+    distance:''
   });
-  const { id } = useParams();
-
   useEffect(() => {
-    axios
-      .get(http://localhost:8070/customer/getcus/${id})
-      .then((response) => {
-        const userData = response.data.user;
-        const updatedDetails = {
-          name: userData.name || "",
-          age: userData.age || "",
-          type: userData.type || "",
-          address: userData.address || "", 
-          drivingExperience: userData.drivingExperiance || "",
-          licenseYear: userData.liscenceYear || "", 
-        };
-        setUserDetails(updatedDetails);
-        console.log(response);
-      })
-      .catch((err) => {
-        console.error("Error fetching user details:", err.response);
-      });
-  }, [id]);
-  
-
- 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserDetails((prevDetails) => ({
-      ...prevDetails,
-      [name]: value,
-    }));
-  };
-  
-  const handleUpdate = () => {
-    axios
-      .put(
-        http://localhost:8070/customer/updatecus/${id},
-        {
-          name: userDetails.name,
-          age: userDetails.age,
-          type: userDetails.type,
-          address: userDetails.address,
-          drivingExperience: userDetails.drivingExperience || "",
-          licenseYear: userDetails.licenseYear || "",
+    const fetchData = async () => {
+      try {
+        let tripData = {};
+        if (location.state) {
+          tripData = location.state; 
+        } else {
+          const response = await Axios.get(`http://localhost:8070/trip/get/${id}`);
+          tripData = response.data.trip;
         }
-      )
-      .then((response) => {
-        console.log("User details updated successfully:", response);
-      })
-      .catch((err) => {
-        console.error("Error updating user details:", err.response);
-      });
-  };
-  return (
-    <div>
-       <Navbar />
-    <div className="container">
-      
-      <h1>Update Profile</h1>
+        setValues(prevValues => ({
+          ...prevValues,
+          Firstname: tripData.Firstname ,
+          Lastname: tripData.Lastname ,
+          Address: tripData.Address ,
+          ContactNo: tripData.ContactNo ,
+          Destination: tripData.Destination ,
+          VisitingPlaces: tripData.VisitingPlaces ,
+          NumberOfPassengers: tripData.NumberOfPassengers ,
+          VehicleType: tripData.VehicleType ,
+          date: tripData.date ,
+          time: tripData.time ,
+          distance: tripData.distance,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    fetchData();
+  }, [id,  location.state]);
+  
 
-      <div className="form-container">
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={userDetails.name || ""}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Age:</label>
-          <input
-            type="number"
-            name="age"
-            value={userDetails.age || ""}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Type:</label>
-          <select
-            name="type"
-            value={userDetails.type || ""}
-            onChange={handleChange}
-          >
-            <option value="Customer">Customer</option>
-            <option value="Driver">Driver</option>
-          </select>
-        </div>
-        <div>
-          <label>Address:</label>
-          <input
-            type="text"
-            name="address"
-            value={userDetails.address || ""}
-            onChange={handleChange}
-          />
-        </div>
-        {userDetails.type === "Driver" && (
-          <div>
-            <label>Driving Experience:</label>
-            <input
-              type="text"
-              name="drivingExperience"
-              value={userDetails.drivingExperience || ""}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        {userDetails.type === "Driver" && (
-          <div>
-            <label>License Year:</label>
-            <input
-              type="text"
-              name="licenseYear"
-              value={userDetails.licenseYear || ""}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        <button onClick={handleUpdate}>Update</button>
-      </div>
-    </div>
- </div>
-);
+const handleUpdate=async()=>{
+  try{
+    await Axios.put(`http://localhost:8070/trip/update/${id}`, values);
+    window.location.href='/';
+  }catch(error){
+    console.log(error)
+  }
 }
+
+  return (
+    <form className='form'>
+    <div className="container4">
+          <div className="form-group">
+            <label htmlFor="Firstname">First Name</label>
+            <input
+                type="text"
+                className="form-control"
+                id="fname"
+                placeholder="Enter customer firstname"
+                 value={values.Firstname}
+            onChange={(e) => setValues({...values, Firstname: e.target.value})}
+          />
+  
+          </div>
+          <div className="form-group">
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lname"
+              placeholder="Enter customer lastname"
+              value={values.Lastname}
+              onChange={(e) => setValues({...values, Lastname: e.target.value})}
+          />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              placeholder="Enter customer address"
+              value={values.Address}
+              onChange={(e) => setValues({...values, Address: e.target.value})}
+
+            />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="contactno">Contact No</label>
+            <input
+              type="tel"
+              className="form-control"
+              id="contactno"
+              placeholder="Enter contact number"
+              value={values.ContactNo}
+              onChange={(e) => setValues({...values, ContactNo: e.target.value})}
+
+
+            />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="destination">Destination</label>
+            <input
+              type="text"
+              className="form-control"
+              id="destination"
+              placeholder="Enter the destination"
+              value={values.Destination}
+              onChange={(e) => setValues({...values, Destination: e.target.value})}
+
+            />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="vplaces">Visiting Places</label>
+            <input
+              type="text"
+              className="form-control"
+              id="vplaces"
+              placeholder="Enter visiting places"
+              value={values.VisitingPlaces}
+              onChange={(e) => setValues({...values, VisitingPlaces: e.target.value})}
+
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="passengers">NumberOfPassengers</label>
+            <select
+              className="form-control"
+              id="passengers"
+              value={values.NumberOfPassengers}      
+              onChange={(e) => setValues({...values, NumberOfPassengers: e.target.value})}
+
+          >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+            </select>
+
+          </div>
+          <div className="form-group col-md-4 new">
+            <label htmlFor="inputVehicleType">VehicleType</label>
+            <select
+              id="inputVehicleType"
+              className="form-control"
+              value={values.VehicleType}        
+              onChange={(e) => setValues({...values, VehicleType: e.target.value})}
+      
+            >
+              <option value="">Choose...</option>
+              <option value="Car">Car</option>
+              <option value="MiniVan">MiniVan</option>
+              <option value="van">van</option>
+              <option value="KDH">KDH</option>
+              <option value="Jeep">Jeep</option>
+              <option value="Bus">Bus</option>
+            </select>
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="date"> Date</label>
+            <input
+              type="date"
+              className="form-control"
+              id="dataepicker"
+              placeholder="Select Date"
+              value={values.date}
+              onChange={(e) => setValues({...values, date: e.target.value})}
+
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="time"> Time</label>
+            <input
+              type="time"
+              className="form-control"
+              id="timepicker"
+              placeholder="Select Time"
+              value={values.time}
+              onChange={(e) => setValues({...values, time: e.target.value})}
+
+
+              
+            />
+          </div>
+          <div className="form-group">
+          <label htmlFor="distance">Distance (in km)</label>
+          <input
+            type="text"
+            className="form-control"
+            id="distance"
+            placeholder="Enter distance traveled"
+            value={values.distance}            
+            onChange={(e) => setValues({...values, distance: e.target.value})}
+
+          />
+        </div>
+
+        <div className='uButton'>
+        <button type="button" className="btn btn-primary" onClick={handleUpdate}>
+        Update
+      </button>
+        </div>
+          </div>
+          </form>
+  )
+}
+export default Update;
